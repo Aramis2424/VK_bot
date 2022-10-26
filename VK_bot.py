52af26c2821d316f3b86874da72dbf426056f5ba
Aramis2424 <srd2424@yandex.ru>
2022-10-26 14:32:27 +0300

    made first step

---
+++ VK_bot.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        VK_bot_test
+# Purpose:      Отладка чат бота
+#
+# Author:      roman2424
+#
+# Created:     26.10.2022
+# Copyright:   (c) roman2424 2022
+#-------------------------------------------------------------------------------
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+MY_TOKEN = vk1.a.HFe_dqoqVDcBfMofcWOMsiPly8ScMvFDWliKD70tLjFnzzK5tZEKDC1wYFfe2OgvvKknDFbfONFhjZLoFlXdHXNBHxNE79IZrOnB2qBg1gKJ18MxuZxLCpU96mt9sxuEydDJTp3WKxXYbvaEtwPbKu1ae6AJtHHVc_n6RqgJW7YEgKBQKlWrFIksDXXFDxDj9AMLrOO9Y817CPRHOznBjQ
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
